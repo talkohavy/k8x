806fb46c6bfe07e04105225c792cf61f0efcf03f
Tal Kohavy <talkohavy@gmail.com>
2024-09-21 09:43:38 +0300

    no more absolute paths

--- src/commands/switchContext.ts
+++ src/commands/switchContext.ts
@@ -1,5 +1,5 @@
 import { execSync } from 'child_process';
-import { inquireNextContext } from '@src/utils/inquireNextContext';
+import { inquireNextContext } from '../utils/inquireNextContext.js';
 
 async function switchContext(currentContext: string = 'b45ck_lmn') {
   const contextToUse = await inquireNextContext(currentContext);
