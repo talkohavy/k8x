9791d9eba1188e8cda577cdcd9b27725f73e1aae
Tal Kohavy <talkohavy@gmail.com>
2024-09-21 09:41:12 +0300

    typechecking now passes

--- src/colors.ts
+++ src/colors.ts
@@ -1,5 +1,5 @@
-const color = (num) => `\x1b[${num}m%s\x1b[0m`;
-const wrapIn = (num, str) => `\x1b[${num}m${str}\x1b[0m`;
+const color = (num: number) => `\x1b[${num}m%s\x1b[0m`;
+const wrapIn = (num: number, str: string) => `\x1b[${num}m${str}\x1b[0m`;
 
 export const COLORS = {
   black: color(30),
@@ -8,9 +8,9 @@ export const COLORS = {
   bright: color(1),
   cyan: color(36),
   green: color(32),
-  magneta: color(35),
+  magenta: color(35),
   red: color(31),
   white: color(37),
   yellow: color(33),
-  underscore: (str) => wrapIn(4, str),
+  underscore: (str: string) => wrapIn(4, str),
 };
