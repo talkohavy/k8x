93fb7304b032f561d902b506054287d62f547c77
Tal Kohavy <talkohavy@gmail.com>
2024-11-05 21:10:29 +0200

    do not fail when exiting the cli tool

--- src/commands/switchContext.ts
+++ src/commands/switchContext.ts
@@ -2,11 +2,15 @@ import { execSync } from 'child_process';
 import { inquireNextContext } from '../utils/inquireNextContext.js';
 
 async function switchContext(currentContext: string = 'b45ck_lmn') {
-  const contextToUse = await inquireNextContext(currentContext);
+  try {
+    const contextToUse = await inquireNextContext(currentContext);
 
-  if (contextToUse === null) return;
+    if (contextToUse === null) return;
 
-  console.log('\n', execSync(`kubectl config use-context ${contextToUse}`).toString());
+    console.log('\n', execSync(`kubectl config use-context ${contextToUse}`).toString());
+  } catch (_error) {
+    console.log('⭐️ Reach for the stars 🚀');
+  }
 }
 
 export { switchContext };
