55f4171cc853c8f68f2eb5c44462735a062cf5e7
Tal Kohavy <talkohavy@gmail.com>
2025-06-25 01:38:54 +0300

    build config updated!

--- build.config.js
+++ build.config.js
@@ -2,21 +2,29 @@ import { execSync } from 'child_process';
 import fs, { cpSync } from 'fs';
 import os from 'os';
 import path from 'path';
+import { build } from 'esbuild';
 
 /**
  * @typedef {{
  *   version: string,
  *   private?: string | boolean,
  *   main: string,
+ *   type: 'module' | 'commonjs'
  *   types: string,
  *   scripts?: Record<string, string>,
  *   publishConfig: {
  *     access: string
  *   },
+ *   devDependencies?: Record<string, string>,
  * }} PackageJson
  */
 
 const ROOT_PROJECT = process.cwd();
+const mode = process.env.NODE_ENV;
+const isProd = mode === 'production';
+const greenColor = '[32m';
+const blueColor = '[34m';
+const stopColor = '[39m';
 
 const outDirName = 'dist';
 
@@ -25,17 +33,17 @@ buildPackageConfig();
 async function buildPackageConfig() {
   cleanDistDirectory();
 
-  buildWithTsc();
+  await runBuild();
 
   copyStaticFiles();
 
   manipulatePackageJsonFile(); // <--- must come AFTER copy of static files
 
-  console.log(`${os.EOL}[34mDONE !!![39m${os.EOL}`);
+  console.log(`${os.EOL}${blueColor}DONE !!!${stopColor}${os.EOL}`);
 }
 
 function cleanDistDirectory() {
-  console.log(`[32m- Step 1:[39m clear the ${outDirName} directory`);
+  console.log(`${greenColor}- Step 1:${stopColor} clear the ${outDirName} directory`);
   if (os.platform() === 'win32') {
     execSync(`rd /s /q ${outDirName}`);
   } else {
@@ -43,37 +51,35 @@ function cleanDistDirectory() {
   }
 }
 
-function buildWithTsc() {
-  console.log('[32m- Step 2:[39m build the output dir');
-  execSync('tsc -p ./tsconfig.json');
-}
+async function runBuild() {
+  console.log(`${greenColor}- Step 2:${stopColor} build the output dir`);
 
-function manipulatePackageJsonFile() {
-  console.log('[32m- Step 5:[39m copy & manipulate the package.json file');
-
-  const packageJsonPath = path.resolve(ROOT_PROJECT, outDirName, 'package.json');
-
-  // Step 1: get the original package.json file
-  /** @type {PackageJson} */
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
-
-  // Step 2: Remove all scripts
-  delete packageJson.scripts;
-  console.log('  • [34mdeleted[39m `scripts` key');
-
-  // Step 3: Change from private to public
-  delete packageJson.private;
-  packageJson.publishConfig.access = 'public';
-  console.log('  • [34mchanged[39m from private to public');
-  console.log('  • [34mchanged[39m publishConfig access to public');
-
-  // Step 4: create new package.json file in the output folder
-  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson));
-  console.log('  • [34mpackage.json[39m file written successfully!');
+  await build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    outfile: `${outDirName}/index.js`,
+    sourcemap: !isProd, // <--- defaults to `false`. for 'node', create sourcemaps is for development only.
+    minify: isProd, // <--- defaults to `false`. should be `true` only in production.
+    platform: 'node', // <--- defaults to 'browser'. If you're creating a CLI tool, use 'node' value. Setting platform to 'node' is beneficial when for example, all packages that are built-in to node such as fs are automatically marked as external so esbuild doesn't try to bundle them.
+    format: 'esm', // <--- When platform is set to 'node', this defaults to 'cjs'.
+    tsconfig: 'tsconfig.json', // <--- Normally the build API automatically discovers tsconfig.json files and reads their contents during a build. However, you can also configure a custom tsconfig.json file to use instead. This can be useful if you need to do multiple builds of the same code with different settings.
+    treeShaking: true, // <--- defaults to `true`. Removes dead code.
+    mainFields: ['main', 'module'], // <--- When platform is set to 'node', this defaults to 'module','main'. When platform is set to 'browser', this defaults to 'browser','module','main'. IMPORTANT! The order matters! 'main', 'module' is not the same as 'module', 'main'! I chose the more risky one, that attempts to tree-shake, but could potentially fail.
+    packages: 'external', // <--- You also may not want to bundle your dependencies with esbuild. There are many node-specific features that esbuild doesn't support while bundling such as __dirname, import.meta.url, fs.readFileSync, and *.node native binary modules. You can exclude all of your dependencies from the bundle by setting packages to external. If you do this, your dependencies must still be present on the file system at run-time since they are no longer included in the bundle.
+    conditions: [], // <--- If no custom conditions are configured, the Webpack-specific module condition is also included. The module condition is used by package authors to provide a tree-shakable ESM alternative to a CommonJS file without creating a dual package hazard. You can prevent the module condition from being included by explicitly configuring some custom conditions (even an empty list).
+    /**
+     * Some npm packages you want to use may not be designed to be run in the browser.
+     * Sometimes you can use esbuild's configuration options to work around certain issues and successfully
+     * bundle the package anyway. Undefined globals can be replaced with either the define feature in
+     * simple cases or the inject feature in more complex cases.
+     */
+    // define :
+    // inject :
+  });
 }
 
 function copyStaticFiles() {
-  console.log('[32m- Step 3:[39m copy static files');
+  console.log(`${greenColor}- Step 3:${stopColor} copy static files`);
 
   const filesToCopyArr = [
     { filename: 'package.json', sourceDirPath: [], destinationDirPath: [] },
@@ -96,4 +102,26 @@ function copyStaticFiles() {
       throw new Error('File MUST exists in order to PASS build process! cp operation failed...');
     }
   });
+}
+
+function manipulatePackageJsonFile() {
+  console.log(`${greenColor}- Step 5:${stopColor} copy & manipulate the package.json file`);
+
+  const packageJsonPath = path.resolve(ROOT_PROJECT, outDirName, 'package.json');
+
+  // Step: get the original package.json file
+  /** @type {PackageJson} */
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
+
+  delete packageJson.private;
+  delete packageJson.scripts;
+  delete packageJson.devDependencies;
+  packageJson.publishConfig.access = 'public';
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson));
+
+  console.log(`  • ${blueColor}changed${stopColor} from private to public`);
+  console.log(`  • ${blueColor}deleted${stopColor} "scripts" key`);
+  console.log(`  • ${blueColor}deleted${stopColor} "devDependencies" key`);
+  console.log(`  • ${blueColor}changed${stopColor} publishConfig access to public`);
+  console.log(`  • ${blueColor}package.json${stopColor} file written successfully!`);
 }
